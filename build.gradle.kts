6b9331f498ff9dbd4cf0eff6597a604f009369a0
Ahnyeongjun <toinin@naver.com>
2025-04-07 10:08:14 +0900

    [INIT] 프로젝트 재생성

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
+
+    // QueryDSL
+    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
+
+    // JPA Entity에 no-arg 생성자 추가
+    kotlin("plugin.noarg") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
+}
+
+group = "com.hbd"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // QueryDSL
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.batch:spring-batch-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+// QueryDSL 설정
+val querydslDir = layout.buildDirectory.dir("generated/querydsl")
+
+kapt {
+    arguments {
+        arg("querydsl.entityAccessors", "true")
+        arg("querydsl.useFields", "true")
+    }
+}
+
+sourceSets["main"].java {
+    srcDir(querydslDir)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "21"
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+// JPA Entity용 no-arg 생성자 자동 추가
+noArg {
+    annotation("jakarta.persistence.Entity")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/hbd/book_be/BookBeApplication.kt
@@ -0,0 +1,11 @@
+package com.hbd.book_be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookBeApplication>(*args)
+}
