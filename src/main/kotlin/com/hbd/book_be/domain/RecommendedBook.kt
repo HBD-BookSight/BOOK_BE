daeae01d3a272e8be52f2ae9ac8ede18d3b5add2
moon <moon@vessl.ai>
2025-04-17 01:28:09 +0900

    Edit RecommendedBook Entity - remove orphanRemoval option - edit LocalDateTime to LocalDate

--- src/main/kotlin/com/hbd/book_be/domain/RecommendedBook.kt
+++ src/main/kotlin/com/hbd/book_be/domain/RecommendedBook.kt
@@ -1,10 +1,12 @@
 package com.hbd.book_be.domain
 
 import jakarta.persistence.*
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Entity
-@Table(name = "recommended_book",
+@Table(
+    name = "recommended_book",
     indexes = [
         Index(name = "idx_recommended_book_created_at", columnList = "created_at"),
         Index(name = "idx_recommended_book_recommended_date", columnList = "recommended_date")
@@ -14,13 +16,13 @@ class RecommendedBook(
     @Id
     var isbn: String,
 
-    @MapsId(value="isbn")
-    @OneToOne(targetEntity = Book::class, fetch = FetchType.LAZY, orphanRemoval = true)
-    @JoinColumn(name="isbn")
-    var book: Book? = null,
+    @MapsId(value = "isbn")
+    @OneToOne(targetEntity = Book::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "isbn")
+    var book: Book,
 
     @Column(name = "recommended_date", nullable = false)
-    val recommendedDate: LocalDateTime,
+    val recommendedDate: LocalDate,
 
     @Column(name = "created_at", nullable = false)
     val createdAt: LocalDateTime = LocalDateTime.now()
