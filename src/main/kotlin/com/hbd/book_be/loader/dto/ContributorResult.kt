f20f7943c0fd59e9e96f85d55bf33970b928db0f
Ahnyeongjun <toinin@naver.com>
2025-04-24 09:28:17 +0900

    [Edit] 불필요 파일 제거

--- src/main/kotlin/com/hbd/book_be/loader/dto/ContributorResult.kt
+++
@@ -1,8 +0,0 @@
-package com.hbd.book_be.loader.dto
-
-import com.hbd.book_be.domain.Author
-
-data class ContributorResult(
-    val authors: MutableList<Author>,
-    val translators: MutableList<String>
-)
