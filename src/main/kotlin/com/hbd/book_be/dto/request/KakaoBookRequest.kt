d8512fb5e9cce9da560f9cf3b5e30f14fb525801
Ahnyeongjun <toinin@naver.com>
2025-04-28 09:33:05 +0900

    [Add] kakoBookApi dto

---
+++ src/main/kotlin/com/hbd/book_be/dto/request/KakaoBookRequest.kt
@@ -0,0 +1,13 @@
+package com.hbd.book_be.dto.request
+
+import com.hbd.book_be.enums.SortType
+
+data class KakaoBookRequest(
+    val query: String,
+    val sort: SortType = SortType.ACCURACY,
+    val page: Int? = 1,
+    val size: Int? = 10,
+    
+    // 검색 필드 제한
+    val target: String? = null
+)
---
+++ src/main/kotlin/com/hbd/book_be/enums/KaKaoSortType.kt
@@ -0,0 +1,6 @@
+package com.hbd.book_be.enums
+
+enum class SortType(val value: String) {
+    ACCURACY("accuracy"),
+    LATEST("latest")
+}
