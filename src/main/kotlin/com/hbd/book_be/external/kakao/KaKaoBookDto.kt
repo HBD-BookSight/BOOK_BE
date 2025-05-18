b8a176770d6fa04601e6bcf9e1204e26ee6dfb07
Ahnyeongjun <toinin@naver.com>
2025-05-18 19:46:05 +0900

    [FIX] KakaoBookDto 구조 개선 및 DB 의존성 제거

--- src/main/kotlin/com/hbd/book_be/external/kakao/KaKaoBookDto.kt
+++ src/main/kotlin/com/hbd/book_be/external/kakao/KaKaoBookDto.kt
@@ -1,17 +1,33 @@
 package com.hbd.book_be.external.kakao
 
-import com.hbd.book_be.domain.Book
-import com.hbd.book_be.dto.BookDto
+import java.time.LocalDateTime
 
 data class KakaoBookDto(
-    val isExist: Boolean,
-    val bookDto: BookDto
+    val isExist: List<Boolean>,
+    val isbn: String,
+    val title: String,
+    val summary: String,
+    val publishedDate: LocalDateTime,
+    val titleImage: String?,
+    val authorList: List<String>,
+    val translator: List<String>?,
+    val price: Int?,
+    val publisher: String,
 ) {
     companion object {
-        fun fromEntity(book: Book, isExist: Boolean): KakaoBookDto {
+        fun fromKakaoApi(document: KakaoApiResponse.Document, isExist: List<Boolean>): KakaoBookDto {
+            val publishedDate = LocalDateTime.parse(document.datetime.substring(0, 19))
             return KakaoBookDto(
                 isExist = isExist,
-                bookDto = BookDto.fromEntity(book)
+                isbn = document.isbn.split(" ").first(),
+                title = document.title,
+                summary = document.contents,
+                publishedDate = publishedDate,
+                titleImage = document.thumbnail,
+                authorList = document.authors,
+                translator = document.translators,
+                price = document.price,
+                publisher = document.publisher,
             )
         }
     }
--- src/main/kotlin/com/hbd/book_be/external/kakao/KakaoBookService.kt
+++ src/main/kotlin/com/hbd/book_be/external/kakao/KakaoBookService.kt
@@ -1,26 +1,20 @@
 package com.hbd.book_be.external.kakao
 
-import com.hbd.book_be.domain.Author
-import com.hbd.book_be.domain.Publisher
 import com.hbd.book_be.dto.AuthorDto
 import com.hbd.book_be.dto.BookDto
 import com.hbd.book_be.dto.PublisherDto
 import com.hbd.book_be.dto.request.BookCreateRequest
-import com.hbd.book_be.exception.ErrorCodes
 import com.hbd.book_be.exception.KakaoBookInfoNotFoundException
 import com.hbd.book_be.exception.NotFoundException
-import com.hbd.book_be.exception.ValidationException
 import com.hbd.book_be.repository.AuthorRepository
 import com.hbd.book_be.repository.BookRepository
 import com.hbd.book_be.repository.PublisherRepository
 import com.hbd.book_be.service.BookService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import kotlin.jvm.optionals.getOrNull
 
 @Service
 class KakaoBookService(
@@ -37,13 +31,23 @@ class KakaoBookService(
 
     @Transactional(readOnly = true)
     fun searchBook(request: KakaoApiRequest): List<KakaoBookDto> {
-        val pageRequest = PageRequest.of(request.page, request.size)
+
         val response = kakaoBookSearchClient.searchBook(request)
             ?: throw KakaoBookInfoNotFoundException(
-                message = "Kakao API에서 책 정보를 찾을 수 없습니다.",
+                message = "Kakao API에서 책 정보를 찾을 수 없습니다."
             )
-        val globalIndex = pageRequest.pageNumber * pageRequest.pageSize + 1L
-        return mapToBookDtos(response.documents, globalIndex)
+
+        val isbnList = response.documents.flatMap { it.isbn.split(" ") }
+        val existingIsbns = bookRepository.findByIsbnIn(isbnList).map { it.isbn }.toSet()
+
+        return response.documents.map { document ->
+            val isExistList = document.isbn.split(" ").map { existingIsbns.contains(it) }
+
+            KakaoBookDto.fromKakaoApi(
+                document = document,
+                isExist = isExistList
+            )
+        }
     }
 
     @Transactional
@@ -60,34 +64,6 @@ class KakaoBookService(
 
         val bookCreateRequest = mapToBookCreateRequest(response.documents.first())
         return bookService.createBook(bookCreateRequest)
-    }
-
-    private fun mapToBookDtos(documents: List<KakaoApiResponse.Document>, startId: Long): List<KakaoBookDto> {
-        val isbnList = documents.map { it.isbn }
-        val existingIsbns = bookRepository.findByIsbnIn(isbnList).map { it.isbn }.toSet()
-
-        return documents.map { document ->
-
-            val authorList = getAuthorList(document)
-            val publisher = getPublisher(document)
-
-            val book = BookDto(
-                isbn = document.isbn,
-                title = document.title,
-                summary = document.contents,
-                publishedDate = parseDate(document.datetime),
-                titleImage = document.thumbnail,
-                authorList = authorList,
-                translator = document.translators,
-                price = document.price,
-                publisher = publisher
-            )
-
-            KakaoBookDto(
-                bookDto = book,
-                isExist = existingIsbns.contains(document.isbn)
-            )
-        }
     }
 
     private fun mapToBookCreateRequest(document: KakaoApiResponse.Document): BookCreateRequest {
