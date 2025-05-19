d4bb8b37dd3393a680654e15c84fdf44fc3f7859
moon <moon@vessl.ai>
2025-05-19 15:03:40 +0900

    Fix prefix of kakaoapi controller

--- src/main/kotlin/com/hbd/book_be/controller/admin/KakaoBookController.kt
+++ src/main/kotlin/com/hbd/book_be/controller/admin/KakaoBookController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/kakao/books")
+@RequestMapping("/admin/v1/kakao/books")
 class KakaoBookController(
     @Autowired
     private val kakaoBookService: KakaoBookService
